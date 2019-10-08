ae1ef4c7a361a1d04fa41c4e83766ec7b60f5d9b
thaonguyenvan <thaonvhanu@gmail.com>
2019-10-08 10:40:37 +0700

    update script

--- ThaoNV/Tim hieu OpenStack/docs/image-create/script/active-key.ps1
+++ ThaoNV/Tim hieu OpenStack/docs/image-create/script/active-key.ps1
@@ -1,5 +1,9 @@
+param(
+[string]$key
+)
+
 $all_services = Get-WmiObject -Query "SELECT * FROM SoftwareLicensingService"
-$all_services.InstallProductKey("VPJ6B-W2R9M-7KD7R-8KWQV-G4TG2")
+$all_services.InstallProductKey($key)
 $service = Get-WmiObject SoftwareLicensingProduct | Where-Object {$_.PartialProductKey}
 $service.Activate()
 $all_services.RefreshLicenseStatus()
